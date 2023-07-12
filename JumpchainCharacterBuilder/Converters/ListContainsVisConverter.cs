9f564eb3667fc0b4b05f548df457960da6c9d1f3
Age-Of-Ages <FerretDeveloper@gmail.com>
2023-07-11 21:23:29 -0400

    Implemented a more refined check visibility on certain elements

---
+++ JumpchainCharacterBuilder/Converters/ListContainsVisConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JumpchainCharacterBuilder.Converters
+{
+    public class ListContainsVisConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var containedObject = values[0];
+            ICollection? list = values[1] as ICollection;
+            
+            if (containedObject != null && list != null)
+            {
+                foreach (var item in list)
+                {
+                    if (containedObject == item)
+                    {
+                        return Visibility.Visible;
+                    }
+                }
+            }
+
+            return Visibility.Hidden;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
