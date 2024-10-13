9eed686a352338c303a057f277a1e9fb8437fdfc
Age-Of-Ages <FerretDeveloper@gmail.com>
2024-10-13 11:30:34 -0400

    Moved format context menu helper to separate function

--- JumpchainCharacterBuilder/Styles.xaml.cs
+++ JumpchainCharacterBuilder/Styles.xaml.cs
@@ -73,6 +73,67 @@ namespace JumpchainCharacterBuilder
                 textBoxTarget.ContextMenu.Items.Add(cutItem);
                 textBoxTarget.ContextMenu.Items.Add(copyItem);
                 textBoxTarget.ContextMenu.Items.Add(pasteItem);
+            }
+        }
+
+        private void FormattedTextBox_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            if (sender is TextBox textBoxTarget)
+            {
+                int caretIndex, cmdIndex;
+                SpellingError spellingError;
+
+                textBoxTarget.ContextMenu = GetContextMenu();
+                caretIndex = textBoxTarget.CaretIndex;
+
+                cmdIndex = 0;
+                spellingError = textBoxTarget.GetSpellingError(caretIndex);
+                if (spellingError != null)
+                {
+                    foreach (string suggestion in spellingError.Suggestions)
+                    {
+                        MenuItem item = new()
+                        {
+                            Header = suggestion,
+                            FontWeight = FontWeights.Bold,
+                            Command = EditingCommands.CorrectSpellingError,
+                            CommandParameter = suggestion,
+                            CommandTarget = textBoxTarget
+                        };
+                        textBoxTarget.ContextMenu.Items.Insert(cmdIndex, item);
+                        cmdIndex++;
+                    }
+                    Separator separator1 = new();
+                    textBoxTarget.ContextMenu.Items.Insert(cmdIndex, separator1);
+                    cmdIndex++;
+                    MenuItem ignoreItem = new()
+                    {
+                        Header = "Ignore",
+                        Command = EditingCommands.IgnoreSpellingError,
+                        CommandTarget = textBoxTarget
+                    };
+                    textBoxTarget.ContextMenu.Items.Insert(cmdIndex, ignoreItem);
+                    cmdIndex++;
+                    Separator separator2 = new();
+                    textBoxTarget.ContextMenu.Items.Insert(cmdIndex, separator2);
+                }
+
+                MenuItem cutItem = new()
+                {
+                    Command = ApplicationCommands.Cut
+                };
+                MenuItem copyItem = new()
+                {
+                    Command = ApplicationCommands.Copy
+                };
+                MenuItem pasteItem = new()
+                {
+                    Command = ApplicationCommands.Paste
+                };
+
+                textBoxTarget.ContextMenu.Items.Add(cutItem);
+                textBoxTarget.ContextMenu.Items.Add(copyItem);
+                textBoxTarget.ContextMenu.Items.Add(pasteItem);
                 string? binding = BindingOperations.GetBinding(textBoxTarget, TextBox.TextProperty).Path.Path;
                 if (binding != null)
                 {
